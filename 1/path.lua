0668b2096db65773fae08fb521d54f1ec71489de
Jin Park <jin+github@jinpark.net>
2016-12-19 21:09:49 +0900

    localize it all

--- 1/path.lua
+++ 1/path.lua
@@ -1,16 +1,16 @@
-require "pl"
+local pl = require "pl"
 
-inputs = "R5, L2, L1, R1, R3, R3, L3, R3, R4, L2, R4, L4, R4, R3, L2, L1, L1, R2, R4, R4, L4, R3, L2, R1, L4, R1, R3, L5, L4, L5, R3, L3, L1, L1, R4, R2, R2, L1, L4, R191, R5, L2, R46, R3, L1, R74, L2, R2, R187, R3, R4, R1, L4, L4, L2, R4, L5, R4, R3, L2, L1, R3, R3, R3, R1, R1, L4, R4, R1, R5, R2, R1, R3, L4, L2, L2, R1, L3, R1, R3, L5, L3, R5, R3, R4, L1, R3, R2, R1, R2, L4, L1, L1, R3, L3, R4, L2, L4, L5, L5, L4, R2, R5, L4, R4, L2, R3, L4, L3, L5, R5, L4, L2, R3, R5, R5, L1, L4, R3, L1, R2, L5, L1, R4, L1, R5, R1, L4, L4, L4, R4, R3, L5, R1, L3, R4, R3, L2, L1, R1, R2, R2, R2, L1, L1, L2, L5, L3, L1"
+local inputs = "R5, L2, L1, R1, R3, R3, L3, R3, R4, L2, R4, L4, R4, R3, L2, L1, L1, R2, R4, R4, L4, R3, L2, R1, L4, R1, R3, L5, L4, L5, R3, L3, L1, L1, R4, R2, R2, L1, L4, R191, R5, L2, R46, R3, L1, R74, L2, R2, R187, R3, R4, R1, L4, L4, L2, R4, L5, R4, R3, L2, L1, R3, R3, R3, R1, R1, L4, R4, R1, R5, R2, R1, R3, L4, L2, L2, R1, L3, R1, R3, L5, L3, R5, R3, R4, L1, R3, R2, R1, R2, L4, L1, L1, R3, L3, R4, L2, L4, L5, L5, L4, R2, R5, L4, R4, L2, R3, L4, L3, L5, R5, L4, L2, R3, R5, R5, L1, L4, R3, L1, R2, L5, L1, R4, L1, R5, R1, L4, L4, L4, R4, R3, L5, R1, L3, R4, R3, L2, L1, R1, R2, R2, R2, L1, L1, L2, L5, L3, L1"
 
-commands = stringx.split(inputs, ", ")
-current_location = {0,0}
-directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
-current_direction = 1 -- 1 is north, 2 is east, 3 is south, 4 is west
+local commands = stringx.split(inputs, ", ")
+local current_location = {0,0}
+local directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+local current_direction = 1 -- 1 is north, 2 is east, 3 is south, 4 is west
 
 
 for key, command in ipairs(commands) do
-    direction = string.sub(command, 1, 1)
-    length = tonumber(string.sub(command, 2))
+    local direction = string.sub(command, 1, 1)
+    local length = tonumber(string.sub(command, 2))
     if direction == 'R' then
         if current_direction == 4 then
             current_direction = 1
