8a07e11e6ed9d0c14e32a5436089c367ac751c54
Jin Park <jin+github@jinpark.net>
2016-12-19 21:42:53 +0900

    local!

--- 3/triangles.lua
+++ 3/triangles.lua
@@ -1,14 +1,14 @@
 local pl = require 'pl'
 
-inputs = file.read('./input.txt')
-valid_triangles = 0
-triangle_list = stringx.split(inputs, '\n')
+local inputs = file.read('./input.txt')
+local valid_triangles = 0
+local triangle_list = stringx.split(inputs, '\n')
 
 for key, triangle in ipairs(triangle_list) do 
-    strip_triangle = stringx.strip(triangle)
+    local strip_triangle = stringx.strip(triangle)
     if string.len(strip_triangle) > 0 then 
-        triangle_sides = stringx.split(strip_triangle)
-        triangle_number_sides = {}
+        local triangle_sides = stringx.split(strip_triangle)
+        local triangle_number_sides = {}
         for i = 1, #triangle_sides do 
             local side = triangle_sides[i]
             triangle_number_sides[i] = tonumber(side)
